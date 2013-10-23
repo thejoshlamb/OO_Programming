ea3e0b190ca07fa932fc74eae1eecc1c6995ca43
Josh Lamb <joshlamb@gmail.com>
2013-10-23 15:38:47 -0400

    Built item and tax basic structure

--- salestax.rb
+++ salestax.rb
@@ -5,14 +5,45 @@
 # calculate total tax
 # return list of items with tax
 
-class Reciept
+# 1 book at 12.49 1 music CD at 14.99 1 chocolate bar at 0.85
 
-	def initialize(list)
-		@list = list.split(" ")
-	end
+#defining tax exempt keywords
+$tax_exempt_keywords["book","chocolate","pills"]
+
+class Item
+		attr_accessor :quantity
+		attr_accessor :description
+		attr_accessor :price
+		attr_accessor :basic_tax
+		attr_accessor :import_tax
+
+		def initialize(quantity, description, price)
+			@quantity = quantity
+			@description = description
+			@price = price
+			@basic_tax = 0.0
+			@import_tax = 0.0
+		end
+
+		def tax_exempt?
+			$tax_exempt_keywords.each do |keyword|
+				
+			end
+
+		def calculate_basic_tax
+			@basic_tax = @price * 0.1 unless tax_exempt?
+		end
+
+		#def calculate_import_tax
+		#	@import_tax = @price * 0.05 if imported?
+
+		
+				
 
 end
 
-input1 = Reciept.new("1 book at 12.49 1 music CD at 14.99 1 chocolate bar at 0.85")
-puts Reciept.list
+item1 = Item.new(1,"book",12.49)
+
+
+
 
