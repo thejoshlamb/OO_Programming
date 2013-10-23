2c9551ee006d170e8752aba7c0010b96b789ccc6
Josh Lamb <joshlamb@gmail.com>
2013-10-23 13:50:16 -0400

    Person class and name accesor added

--- people.rb
+++ people.rb
@@ -1,11 +1,30 @@
-class Student
+class Person
+	
+	attr_accessor :name
+
+	def greeting
+		puts "Hi, my name is #{@name}"
+	end
+
+end
+
+class Student < Person
+
 	def learn
 		puts "I get it!"
 	end
+
 end
 
-class Instructor
+class Instructor < Person
+
 	def teach
 		puts "Everything in Ruby is an object."
 	end
+
 end
+
+Josh = Student.new
+Josh.name = "Josh"
+Josh.greeting
+Josh.learn
