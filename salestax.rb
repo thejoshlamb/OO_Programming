731d8a0fabd9bf2e01f5cffeceba7ad24f383388
Josh Lamb <joshlamb@gmail.com>
2013-10-24 11:51:29 -0400

    Solution complete, rounding errors exist

--- salestax.rb
+++ salestax.rb
@@ -1,30 +1,53 @@
-# take list of inputs
-# parse into examinable list
-# calculate basic tax and apply
-# calculate import tax and apply
-# calculate total tax
-# return list of items with tax
+#defining tax exempt keywords before we begin
+$tax_exempt_keywords = ["book","chocolate","pill","kitten"]
 
-# 1 book at 12.49 1 music CD at 14.99 1 chocolate bar at 0.85
+#The reciept class contains the totals for the bill
+class Reciept
+	def initialize(basket)
+		@basket = basket
+		@@total_tax = 0.0
+		@@total_price = 0.0
+	end
 
-#defining tax exempt keywords
-$tax_exempt_keywords = ["book","chocolate","pills"]
+	# calculates taxes for all items, as well as the total for the reciept
+	def calculate_totals
+		@basket.each do |item|
+			item.calculate_tax
+			@@total_price += item.price
+			@@total_price += item.tax
+		end
+	end
 
-class Item
-		attr_accessor :quantity
-		attr_accessor :description
-		attr_accessor :price
-		attr_accessor :basic_tax
-		attr_accessor :import_tax
+	#prints the whole reciept. note that '%.2f' is used to ensure two decimal places are always returned
+	def print_reciept
+		@basket.each { |item| item.display }
+		puts "Sales Tax: $#{('%.2f' % @@total_tax)}" unless @@total_tax == 0.0
+		puts "Total: $#{'%.2f' % @@total_price}"
+	end
+end
+
+# The item class is used for each individual item
+class Item < Reciept
+		attr_accessor :quantity, :description ,:price, :tax
+		#attr_accessor :unitprice
 
 		def initialize(quantity, description, price)
 			@quantity = quantity
 			@description = description
-			@price = price
-			@basic_tax = 0.0
-			@import_tax = 0.0
+			#@unitprice = price
+			@price = price * quantity * 100
+			@tax = 0.0
 		end
 
+		def basic_tax_rate
+			0.10
+		end
+
+		def import_tax_rate
+			0.05
+		end
+
+		#checks to see if the item is exempt for basic tax by iterating through known tax-exempt keywords
 		def tax_exempt?
 			$tax_exempt_keywords.each do |keyword|
 				return true if @description.include?(keyword)
@@ -32,34 +55,55 @@ class Item
 			return false
 		end
 
+		#checks to see if the item is imported by looking for the word "imported"
 		def imported?
-			return false unless @description.include?("imported")
+			return false unless @description.include?("import")
+			return true
 		end
 
-		def calculate_basic_tax
-			@basic_tax = @price * 0.1 unless tax_exempt?
-		end
-
-		def calculate_import_tax
-			@import_tax = @price * 0.05 if imported?
+		#calculates applicable taxes for the item
+		def calculate_tax
+			basic_tax = 0.0
+			import_tax = 0.0
+			basic_tax = @price * basic_tax_rate unless tax_exempt?
+			import_tax = @price * import_tax_rate if imported?
+			#basic_tax = (basic_tax*20).ceil/20
+			#import_tax = (import_tax*20).ceil/20
+			@tax = basic_tax + import_tax
+			@@total_tax += @tax
 		end
 
 		def display
+			puts "#{@quantity} #{{description}}"
 			print "#{@quantity} "
 			print "#{@description}: "
-			print "$#{@price} "
-			print "Sales Taxes: $#{@basic_tax+@import_tax} " unless (@basic_tax+@import_tax == 0)
+			print "$#{'%.2f' % (@price+@tax)} "
+			puts
 		end
 end
 
 item1 = Item.new(1,"book",12.49)
-item2 = Item.new(1,"music CD",10.00)
-item3 = Item.new(1,"chocolate bar",12.49)
-Reciept1 = [item1, item2, item3]
+item2 = Item.new(1,"music CD",14.99)
+item3 = Item.new(1,"chocolate bar",0.85)
+item4 = Item.new(1,"imported box of chocolates",10.00)
+item5 = Item.new(1,"imported bottle of perfume",47.50)
+item6 = Item.new(1,"imported bottle of perfume", 27.99)
+item7 = Item.new(1,"bottle of perfume",18.99)
+item8 = Item.new(1,"packet of headache pills",9.75)
+item9 = Item.new(1,"box of imported chocolates",11.25)
 
-item1.calculate_basic_tax
-item1.calculate_import_tax
-item1.display
+Reciept1 = Reciept.new([item1,item2,item3])
+Reciept1.calculate_totals
+Reciept1.print_reciept
+
+Reciept2 = Reciept.new([item4,item5])
+Reciept2.calculate_totals
+Reciept2.print_reciept
+
+Reciept3 = Reciept.new([item6,item7,item8, item9])
+Reciept3.calculate_totals
+Reciept3.print_reciept
+
 
 
 
